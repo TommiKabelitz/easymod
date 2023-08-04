1690651631ab01a1f8f0e57a2c68252bace7cfa2
Thomas Kabelitz <thomas.kabelitz@adelaide.edu.au>
2023-08-04 18:50:03 +1000

    setuptools requirements

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+setup(
+  name = 'modulepy',
+  packages = ['modulepy'],
+  version = '0.9',
+  license='MIT',
+  description = 'Provides the utility of the module command from within python.',
+  author = 'Tommi Kabelitz',
+  author_email = 'tommi.kabelitz@gmail.com',
+  url = 'https://github.com/TommiKabelitz',
+  download_url = "https://github.com/TommiKabelitz/modulepy/archive/refs/tags/v0.9.tar.gz",
+  keywords = ['Environment', 'Modules'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',    
+    'Programming Language :: Python :: 3.8',    
+    'Programming Language :: Python :: 3.9',    
+    'Programming Language :: Python :: 3.10',    
+    'Programming Language :: Python :: 3.11',    
+  ],
+)
