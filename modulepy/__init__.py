910cfc1d3cca539b14bdcdf99ac2e320c28df071
Thomas Kabelitz <thomas.kabelitz@adelaide.edu.au>
2023-08-04 18:24:16 +1000

    Initial working commit

---
+++ modulepy/__init__.py
@@ -0,0 +1,1 @@
+from modulepy import Module
---
+++ modulepy/modulepy.py
@@ -0,0 +1,235 @@
+import os
+import re
+
+class Module:
+    # Default sub-commands. This is the list of sub-commands in Modules/v4.3.0
+    sub_commands = [
+        "help",
+        "add",
+        "load",
+        "rm",
+        "unload",
+        "swap",
+        "switch",
+        "show",
+        "display",
+        "list",
+        "avail",
+        "aliases",
+        "use",
+        "unuse",
+        "refresh",
+        "reload",
+        "purge",
+        "clear",
+        "source",
+        "whatis",
+        "apropos",
+        "keyword",
+        "search",
+        "test",
+        "save",
+        "restore",
+        "saverm",
+        "saveshow",
+        "savelist",
+        "initadd",
+        "initprepend",
+        "initrm",
+        "initswitch",
+        "initlist",
+        "initclear",
+        "path",
+        "paths",
+        "config",
+    ]
+    # Default environment variables.
+    # This is the list of environment variables in Modules/v4.3.0
+    envinronment_variables = [
+        "LOADEDMODULES",
+        "MODULECONTACT",
+        "MODULEPATH",
+        "MODULERCFILE",
+        "MODULESHOME",
+        "MODULES_AUTO_HANDLING",
+        "MODULES_AVAIL_INDEPTH",
+        "MODULES_CMD",
+        "MODULES_COLLECTION_PIN_VERSION",
+        "MODULES_COLLECTION_TARGET",
+        "MODULES_COLOR",
+        "CLICOLOR",
+        "MODULES_COLORS",
+        "MODULES_IMPLICIT_DEFAULT",
+        "MODULES_LMALTNAME",
+        "MODULES_LMCONFLICT",
+        "MODULES_LMNOTUASKED",
+        "MODULES_LMPREREQ",
+        "MODULES_PAGER",
+        "MODULES_RUN_QUARANTINE",
+        "MODULES_SEARCH_MATCH",
+        "MODULES_SET_SHELL_STARTUP",
+        "MODULES_SILENT_SHELL_DEBUG",
+        "MODULES_SITECONFIG",
+        "MODULES_TERM_BACKGROUND",
+        "MODULES_UNLOAD_MATCH_ORDER",
+        "MODULES_USE_COMPAT_VERSION",
+        "MODULES_VERBOSITY",
+        "_LMFILES_",
+    ]
+
+    def __init__(self, home: os.PathLike = None):
+        """
+        The Module object is a simple interface to the sub-commands and environment variables associated with the module command.
+
+        Upon initialisation, module commands are accessed through the result the Module object as if they were class methods. For example
+
+        >> module = Module()
+        >> module.list()
+        No Modulefiles Currently Loaded.
+        >> module.load("openmpi/4.1.5")
+        >> module.list()
+        Currently Loaded Modulefiles:
+         1) openmpi/4.1.5
+
+
+        Module environment variables may be accessed through the following
+
+        >> module = Module()
+        >> module.environ["MODULE_CMD"]
+        /opt/Modules/v4.3.0/libexec/modulecmd.tcl
+
+        Parameters
+        ----------
+        home : os.PathLike, optional
+            Path to the home directory for the module command, optional, by default None.
+
+        Raises
+        ------
+        ValueError
+            If home not passed, a ValueError is raised if the home directory for the module command cannot be found through the environment variable 'MODULESHOME'.
+        """
+        if home is not None:
+            self.modules_home = home
+        else:
+            try:
+                self.modules_home = os.environ["MODULESHOME"]
+            except KeyError:
+                raise ValueError(
+                    "Cannot automatically set modules_home. Environment variable MODULESHOME unset. Pass module home directory to Module"
+                )
+
+        # Initialise modules command, variables from init script are saved into global
+        # scope here
+        with open(os.path.join(self.modules_home, "init", "python.py"), "r") as f:
+            exec(f.read(), globals())
+        # Grabbing the resulting module function
+        self.module = globals()["module"]
+
+    def parse_man_file(self, man_file: os.PathLike = None):
+        """
+        Parse the module manual file for available module sub-commands and environment variables.
+
+        The manual file is generally located in the module home directory in a file called module.1
+
+        Parameters
+        ----------
+        man_file : os.PathLike, optional
+            Path to the manual file, by default None
+        """
+        self.get_sub_commands(man_file)
+        self.get_environment_variables(man_file)
+
+    def get_sub_commands(self, man_file: os.PathLike = None):
+        """
+        Get list of sub commands to the module command.
+
+        See parse_man_file for information about man_file.
+
+        Parameters
+        ----------
+        man_file : os.PathLike, optional
+            Path to the manual file, by default None
+        """
+
+        # Standard manual file location
+        if man_file is None:
+            man_file = os.path.join(
+                self.modules_home, "share", "man", "man1", "module.1"
+            )
+
+        with open(man_file, "r") as f:
+            man = f.read()
+
+        # Find section of manual re. sub-commands
+        start = man.find(".SS Module Sub\-Commands")
+        end = start + man[start + 3 :].find(".SS") + 1
+
+        sub_commands_section = man[start:end]
+
+        # Regex to extract the subcommands by utilising the formatting/syntax of
+        # manual pages
+        self.sub_commands = re.findall(r".sp\n\\fB([\w|-]+)\\fP", sub_commands_section)
+
+    def get_environment_variables(self, man_file: os.PathLike = None):
+        """
+        Get list of environment variables set by the module command.
+
+        See parse_man_file for information about man_file.
+
+        Parameters
+        ----------
+        man_file : os.PathLike, optional
+            Path to the manual file, by default None
+        """
+        # Standard manual file location
+        if man_file is None:
+            man_file = os.path.join(
+                self.modules_home, "share", "man", "man1", "module.1"
+            )
+
+        with open(man_file, "r") as f:
+            man = f.read()
+
+        start = man.find(".SH ENVIRONMENT")
+        end = start + man[start + 3 :].find("\n.S") + 1
+
+        environment_vars_section = man[start:end]
+
+        # Regex to extract the environment variables by utilising the formatting/syntax of
+        # manual pages
+        self.envinronment_variables = re.findall(
+            r".sp\n\\fB([A-Z|-|_]+)\\fP", environment_vars_section
+        )
+
+    def __getattr__(self, _name):
+        """
+        Use __getattr__ to allow calling of the module functions as
+        if they were methods. See documentation of Module for example use.
+        """
+        # __getattr__ is only called if _name is not already an attribute of
+        # the class.
+        # First check if the attribute is in the list of sub-commands.
+        if _name in self.sub_commands:
+            # Have to return a wrapper function so that args and kwargs
+            # can be passed through
+            def wrapper(*args, **kwargs):
+                return self.module(_name, *args, **kwargs)
+
+            return wrapper
+        else:
+            raise AttributeError("Module has no such attribute %s" % _name)
+
+    @property
+    def environ(self):
+        """
+        Dictionary of environment variables associated with the module command.
+
+        Dictionary updated each time environ is accessed. If environment variables are missing
+        from the dict, they are either unset or missing from self.environment_variables. The
+        latter can be updated using self.get_environment_variables.
+        """
+        return {
+            var: os.environ[var]
+            for var in self.envinronment_variables
+            if os.environ.get(var) is not None
+        }
